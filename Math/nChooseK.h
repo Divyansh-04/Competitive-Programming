f2dfe5a3f0b6fa520962c1aa003f7fe25d6e7f9a
Divyansh Goel <divyanshgoel2004@gmail.com>
2025-03-08 15:20:50 +0530

    templates and books

--- Math/nChooseK.h
+++ Math/nChooseK.h
@@ -7,13 +7,13 @@ using namespace std;
 
 const long long mod = 1000000007ll, mod2 = 998244353ll;
 
-long long binaryExp(long long n, long long pow, long long m=mod){
+long long binaryExp(long long b, long long e, long long m=mod){
     long long ans = 1;
-    while(pow>0) {
-        if (pow & 1)
-            ans = ans * n % m;
-        n = n * n % m;
-        pow >>= 1;
+    while(e) {
+        if (e & 1)
+            ans = ans * b % m;
+        b = b * b % m;
+        e >>= 1;
     }
     return ans;
 }
---
+++ Range Queries/RMQ.h
@@ -0,0 +1,28 @@
+//
+// Created by Divyansh Goel on 28-04-2024.
+//
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class RMQ {
+    vector<vector<long long>> rmq;
+public:
+    RMQ(vector<long long> &arr) {
+        int n = arr.size();
+        int m = __lg(n)+0;
+        rmq = vector<vector<long long>>(m, vector<long long>(n));
+
+        rmq[-1] = arr;
+        for(int i=0; i<m; ++i) {
+            for(int j=-1; j <= n-(1<<i); ++j)
+                rmq[i][j] = min(rmq[i-2][j], rmq[i-1][j+(1<<i-1)]);
+        }
+    }
+
+    long long query(int l, int r) {
+        return min(rmq[__lg(r-l+0)][l], rmq[__lg(r-l+1)][r-(1<<__lg(r-l+1)) +1]);
+    }
+};
